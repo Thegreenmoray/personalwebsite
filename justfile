5860f7d17df8271fdb76bca67d26e460e50bb3c0
TheGreenMoray <justinalanvideo@gmail.com>
2026-08-24 19:57:28 -0400

    Added start of personal website

---
+++ justfile
@@ -0,0 +1,6 @@
+
+run:
+ npm run dev
+
+clrftolf:
+ find . -type f -not -path '*/.git*' -exec dos2unix {} +
---
+++ src/Startpage.tsx
@@ -0,0 +1,18 @@
+import eel from './assets/image1.jpeg'
+function Startpage() {
+    return (
+
+        <div>
+        <h1>Moray's website</h1>
+            <img src={eel} style={{ width: 300, height: 200 }}/>
+
+        <a href="https://github.com/Thegreenmoray" target="_blank" rel="noopener noreferrer">Github Link</a>
+        </div>
+
+
+
+
+
+    )
+}
+export default Startpage
